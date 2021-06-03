8792f36a0a01c7403e694ecadb26dc57fab398b7
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-06-04 00:54:36 +0200

    add an OTP gen_server skeleton for worker

--- src/search_worker.erl
+++ src/search_worker.erl
@@ -1,60 +1,38 @@
 -module(search_worker).
 
--define(POOL_ID, <<"23077202">>).
--define(WAIT_TIMEOUT, 10000).
--define(QUOTE_DATA, [
-    #{url => <<"ya.ru">>, screenshot => <<"123">>},
-    #{url => <<"google.com">>, screenshot => <<"321">>}
-]).
+-behaviour(gen_server).
 
--export([start/1]).
+-export([start_link/2, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2,
+        handle_info/2, code_change/3, terminate/2]).
+%%% API
 
-% % all worker-process needs to know
-% -record(state, {query, search_task_id=undefined}).
+% TODO use Name, City, etc.
+% TODO add budget cap
+start_link(Description, ReportTo) ->
+    gen_server:start_link(?MODULE, {Description, ReportTo}, []).
 
-start(Query) ->
-    spawn(fun() -> init(Query) end).
+stop(Pid) ->
+    gen_server:call(Pid, stop).
 
-init(Query) ->
-    TaskId = create_task(?POOL_ID, Query),
-    io:format("Created a Toloka task in Pool ~p, Task Id ~p~n", [?POOL_ID, TaskId]),
-    wait_for_search(TaskId, 2000).
+%%% OTP Callbacks
 
-wait_for_search(_TaskId, Wait) when Wait > ?WAIT_TIMEOUT ->
-    io:format("I ain't gonna wait ~p seconds!~n", [Wait / 1000]),
-    throw(error);
-wait_for_search(TaskId, Wait) ->
-    io:format("I will sleep for ~p seconds before checking Toloka...~n", [Wait / 1000]),
-    receive
-    after Wait ->
-        case is_ready(TaskId) of
-            true ->
-                io:format("Hooray! Toloka is ready!~n"),
-                download_solution(TaskId);
-            false ->
-                io:format("Not ready yet :-/~n"),
-                wait_for_search(TaskId, Wait * 2)
-        end
-    end.
+init({Description, ReportTo}) ->
+    {ok, {Description, ReportTo}}.
 
-download_solution(_TaskId) ->
-    QuoteData = ?QUOTE_DATA,
-    io:format("There are ~p answers~n", [length(QuoteData)]),
-    io:format("Downloading first..."),
-    ok.
+handle_call(stop, _From, State) ->
+    {stop, normal, ok, State};
+handle_call(_Mst, _From, State) ->
+    {noreply, State}.
 
-% @doc make file appear on yandex disk
-% copy to Yandex.Disk without downloading!
-% upload_to_yandex_disk(MeaningfulName) ->
-%
-    % download files
-    % upload to yandex disk
-    % create check pool
+handle_cast(_Msg, State) ->
+    {noreply, State}.
 
-%%% Private Functions
+handle_info(timeout, State) ->
+    {noreply, State}.
 
-%%% External Toloka
-download_screenshot(_FileId) -> <<1, 2, 3>>.
-create_task(_PoolId, _Query) -> task_id.
-is_ready(_TaskId) -> rand:uniform() > 0.6.
-% download_solution(_TaskId) -> [{url1, screenshot1_id}, {url2, screenshot2_id}].
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+terminate(_Reason, _State) -> ok.
+
