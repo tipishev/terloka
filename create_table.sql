9603c25c2de452aa3d26301e9bb7c9ee1b25adf6
Timofey Tipishev <timofey.tipishev@kivra.com>
2022-03-24 18:24:57 +0100

    start adding initialization

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,11 @@
-CREATE TABLE search_requests (position_id INTEGER PRIMARY KEY, description TEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, result TEXT, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP);
+CREATE TABLE search_requests (
+	id INTEGER PRIMARY KEY AUTOINCREMENT
+	position_id INTEGER NOT NULL,
+	description TEXT NOT NULL,
+	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	result TEXT,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 INSERT INTO search_requests (position_id, description) VALUES (123, "проверка");
 
--- src/python_erlang_exchange.erl
+++ src/python_erlang_exchange.erl
@@ -1,39 +1,48 @@
 -module(python_erlang_exchange).
 
+-export([initialize/0]).
 -export([get_search_requests/0]).
 -export([put_search_result/2]).
 
 -export([test_get/0, test_put/0]).
 
+%%% Macros
+-define(DB_FILENAME, "python_erlang_exchange.sqlite3").
+
 test_get() ->
     get_search_requests().
 
 test_put() ->
-    put_search_result(123, <<"\">>">>).
+    put_search_result(_PositionId = 123, _SearchResult = <<"esqlite3 again">>).
 
 % TODO make it a process
-% TODO subscribe to insert events
+% TODO subscribe to insert events in SQL
+
+initialize() ->
+    {ok, _Conn} = esqlite3:open(?DB_FILENAME).
 
 get_search_requests() ->
-    {ok, Conn} = esqlite3:open("python_erlang_exchange.sqlite3"),
-    Query = "SELECT * from search_requests;",
-    Result = esqlite3:map(fun as_map/1, Query, Conn),
+    {ok, Conn} = esqlite3:open(?DB_FILENAME),
+    Sql = "SELECT * from search_requests;",
+    Result = esqlite3:map(fun as_map/1, Sql,  Conn),
     esqlite3:close(Conn),
     Result.
 
 put_search_result(PositionId, SearchResult) ->
-    {ok, Conn} = esqlite3:open("python_erlang_exchange.sqlite3"),
+    {ok, Conn} = esqlite3:open(?DB_FILENAME),
 
     % FIXME protect against SQL injection
     Template =
         "UPDATE search_requests"
         " SET result = '~s', updated_at = DATETIME()"
         " WHERE position_id=~p;",
-    Query = io_lib:format(Template, [SearchResult, PositionId]),
+    Sql = io_lib:format(Template, [SearchResult, PositionId]),
 
-    esqlite3:q(Query, Conn),
+    esqlite3:q(Sql, Conn),
     esqlite3:close(Conn),
     ok.
+
+%%% Private functions
 
 as_map({PositionId, Description, CreatedAt, Result, UpdatedAt}) ->
     #{
@@ -43,3 +52,6 @@ as_map({PositionId, Description, CreatedAt, Result, UpdatedAt}) ->
         result => Result,
         updated_at => UpdatedAt
     }.
+
+% WriteRead = fun(N) -> python_erlang_exchange:test_put(), io:format("written ~p~n", [N]), python_erlang_exchange:test_get(), io:format("read ~p~n", [N]), N end,
+% [WriteRead(N) || N <- lists:seq(1, 1000)].
